87b7e26e7facef5d65be26ae37706655c1f1fc88
RockyTides <RockyTides@users.noreply.github.com>
2019-08-05 12:00:07 -0400

    models updated

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ class userData(ndb.Model):
     sUsername = ndb.StringProperty(required = True)
     sPassword = ndb.StringProperty(required = True)
     sAge = ndb.IntegerProperty(required = True)
+
+class tweetPost(ndb.Model):
+    Author = nbd.StringProperty(required = True)
+    TweetType = nbd.IntegerProperty(required = True)
+    Caption = nbd.StringProperty()
+    
