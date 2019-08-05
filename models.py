cbbda8ca70b930708523d5d9251f656a50e31e7c
Youssoupha Cisse <youssouphacisse00@gmail.com>
2019-08-05 12:14:42 -0400

    no age

--- models.py
+++ models.py
@@ -3,10 +3,9 @@ from google.appengine.ext import ndb
 class userData(ndb.Model):
     sUsername = ndb.StringProperty(required = True)
     sPassword = ndb.StringProperty(required = True)
-    sAge = ndb.IntegerProperty(required = True)
+    
 
 class tweetPost(ndb.Model):
     Author = nbd.StringProperty(required = True)
     TweetType = nbd.IntegerProperty(required = True)
     Caption = nbd.StringProperty()
-    
