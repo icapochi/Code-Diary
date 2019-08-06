07e310ef21e8e15ae27c1cef60107d91184d32f3
Youssoupha Cisse <youssouphacisse00@gmail.com>
2019-08-06 14:27:51 -0400

    Form working

--- main.py
+++ main.py
@@ -28,14 +28,17 @@ class results(webapp2.RequestHandler):
         result_template = the_jinja_env.get_template('templates/result.html')
         self.response.write(result_template.render({'tweet_info' : tweet_info}))
     def post(self):
-        user = users.get_current_user()
-        nickname = user.nickname()
+        # user = users.get_current_user()
+        # nickname = user.nickname()
         # Use the user input to create a new blog post
         name_input = self.request.get('name')
         description_input = self.request.get('description')
 
-        tweetPost = tweetPost(Author=name_input, Caption=description_input)
-        tweetPost.put()
+        tweetoutput = tweetPost(Author=name_input, Caption=description_input, TweetType=1)
+        tweetoutput.put()
+        tweet_info = tweetPost.query().fetch()
+        result_template = the_jinja_env.get_template('templates/result.html')
+        self.response.write(result_template.render({'tweet_info' : tweet_info}))
 
 # the app configuration section
 app = webapp2.WSGIApplication([
