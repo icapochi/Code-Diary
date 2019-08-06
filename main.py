ca8313c292df30d4b07c1dc14470626f59c3f5d6
Youssoupha Cisse <youssouphacisse00@gmail.com>
2019-08-06 14:14:43 -0400

    Backpost for post handler

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ class results(webapp2.RequestHandler):
         tweet_info = tweetPost.query().fetch()
         result_template = the_jinja_env.get_template('templates/result.html')
         self.response.write(result_template.render({'tweet_info' : tweet_info}))
+    def post(self):
+        user = users.get_current_user()
+        nickname = user.nickname()
+        # Use the user input to create a new blog post
+        name_input = self.request.get('name')
+        description_input = self.request.get('description')
+
+        tweetPost = tweetPost(Author=name_input, Caption=description_input)
+        tweetPost.put()
 
 # the app configuration section
 app = webapp2.WSGIApplication([
