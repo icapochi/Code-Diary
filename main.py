5f574a8411a565f72ab87876dfc3ba16881c75d8
RockyTides <RockyTides@users.noreply.github.com>
2019-08-08 10:45:13 -0400

    Page auto refresh

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from google.appengine.ext import ndb
 from models import userData, tweetPost
 from google.appengine.api import users
 from seedtweets import seed_data
+import time
 
 
 # This initializes the jinja2 Environment.
@@ -22,7 +23,7 @@ class EnterInfoHandler(webapp2.RequestHandler):
     def get(self):  # for a get request
         welcome_template = the_jinja_env.get_template('templates/result.html')
         self.response.write(welcome_template.render())  # the response
-        seed_data()
+
 
 class results(webapp2.RequestHandler):
     def get(self):  # for a get request
@@ -41,14 +42,19 @@ class results(webapp2.RequestHandler):
         # user = users.get_current_user()
         # nickname = user.nickname()
         # Use the user input to create a new blog post
-        name_input = self.request.get('name')
-        description_input = self.request.get('description')
-        currenttime= datetime.datetime.now()
-        tweetoutput = tweetPost(Author=name_input, Caption=description_input, TweetType=1, Creationtime=currenttime)
-        tweetoutput.put()
-        tweet_info = tweetPost.query().order(-tweetPost.Creationtime).fetch()
-        result_template = the_jinja_env.get_template('templates/result.html')
-        self.response.write(result_template.render({'tweet_info' : tweet_info}))
+        user = users.get_current_user()
+        if user:
+            email = user.email()
+            login_url = users.create_login_url('/')
+            name_input = self.request.get('name')
+            description_input = self.request.get('description')
+            currenttime= datetime.datetime.now()
+            tweetoutput = tweetPost(Author=name_input, Caption=description_input, TweetType=1, Creationtime=currenttime)
+            tweetoutput.put()
+            time.sleep(1)
+            tweet_info = tweetPost.query().order(-tweetPost.Creationtime).fetch()
+            result_template = the_jinja_env.get_template('templates/result.html')
+            self.response.write(result_template.render({'tweet_info' : tweet_info, "user" : user, "login_url" : login_url}))
 
 class logged(webapp2.RequestHandler):
     def get(self):
